f8d86c1cc94a989d6c4c2b96eeee5c801806aeb6
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2025-12-13 19:51:19 +0100

    Fix for when x ≈ 1/q
    - prevent over-minimization of the denominator
    - when tol < eps(x), give the most accurate result (don't minimize)

--- src/index.ts
+++ src/index.ts
@@ -23,11 +23,27 @@ const drq = (n: number, d: number): [number, number, number] => {
 }
 
 /**
+ * Ceil division of x / y (x and y both positive)
+ */
+const cld = (x: number, y: number): number => {
+  const r = x % y;
+  return Math.round((x - r)/y) + Number(r > 0);
+}
+
+/**
  * Ceil division of (x + y) / z (x+y and z both positive)
  */
-const cld = ([x, y]: [number, number], z: number): number => {
+const cld2 = ([x, y]: [number, number], z: number): number => {
   const r = ((x % z) + (y % z)) % z;
   return Math.round((x + y - r)/z) + Number(r > 0);
+}
+
+/**
+ * Remainder of x after floor division by y (modulo reduction).
+ * Equivalent to `x - y*⌊x/y⌋` without intermediate rounding.
+ */
+function mod(x: number, y: number) {
+  return ((x % y) + y) % y;
 }
 
 /**
@@ -53,21 +69,33 @@ function rationalize(x: number, tol: number = eps(x)): [number, number] {
     throw TypeError(`x must be a number (received ${typeof x})`);
   }
 
-  if (!(typeof tol === 'number' && tol >= 0)) {
-    const Err = typeof tol === 'number' ? RangeError : TypeError;
-    throw Err(`Tolerance must be a non-negative number`);
+  const sign = Math.sign(x);
+  x = Math.abs(x);
+
+  let epsX = tol;
+  if (arguments[1] !== undefined) {
+    // Custom tolerance is given
+    if (!(typeof tol === 'number' && tol >= 0)) {
+      const Err = typeof tol === 'number' ? RangeError : TypeError;
+      throw Err(`Tolerance must be a non-negative number`);
+    }
+
+    if (x <= tol) {
+      return [sign*0, 1];
+    }
+
+    epsX = eps(x);
   }
 
   if (Number.isInteger(x)) {
-    return [Int54(x), 1];
+    return [sign*Int54(x), 1];
   }
-
-  const sign = Math.sign(x);
-  x = Math.abs(x);
 
   // Compute [p, q] as the convergents of the regular continued fraction
   // representation of x.
   // @see https://github.com/lvlte/rationalize/blob/main/rationale.md
+
+  let n = 0;                  // convergent index
 
   let [p2, p1] = [0, 1]       // [pₙ₋₂, pₙ₋₁]
   let [q2, q1] = [1, 0]       // [qₙ₋₂, qₙ₋₁]
@@ -76,6 +104,7 @@ function rationalize(x: number, tol: number = eps(x)): [number, number] {
   let [e1, e, a] = drq(x, 1); // [|eₙ₋₁|, |eₙ|, aₙ]
 
   while (e > t) {
+    n++;
     [p2, p1] = [p1, Int54(p1*a + p2)];
     [q2, q1] = [q1, Int54(q1*a + q2)];
 
@@ -83,12 +112,34 @@ function rationalize(x: number, tol: number = eps(x)): [number, number] {
     [t1, t] = [t, a*t + t1];
   }
 
-  if (a > 1) {
+  // Having n=0 at this point means tol is greater than the fractional part of x
+  // and the current value of a is ⌊1/x⌋, which is fine given that tol.
+
+  if (n > 0 && a > 1) {
     // There likely exists a semiconvergent between pₙ₋₁/qₙ₋₁ and pₙ/qₙ that
     // satisfies the tolerance. Find smallest `a` to minimize p and q.
-    const e2 = a*e1 + e;
-    const t2 = tol*q2;
-    a = cld([e2, -t2], e1 + t1);
+
+    if (p1 === 0 && t1 <= epsX*2) {
+      // We got an inverse 1/q : in this situation the difference of magnitude
+      // between e1 and t1 is still very high and the floating-point addition
+      // e1 + t1 is not accurate enough.
+      if (t1 < epsX) {
+        // With tolerance less than epsX we actually don't want to minimize `a`.
+        // Since we have a candidate [p, q] = [1, a] with a = ⌊1/x⌋, satisfying
+        // the tolerance, we left `a` untouched except if the ceil div remainder
+        // is smaller than the current error term for `a`.
+        a = -mod(1, -e1) < e ? a + 1 : a;
+      }
+      else {
+        // Otherwise we use epsX instead of t1 to prevent over-minimization.
+        a = Math.min(a, cld(1, e1 + epsX));
+      }
+    }
+    else {
+      const e2 = a*e1 + e;
+      const t2 = tol*q2;
+      a = cld2([e2, -t2], e1 + t1);
+    }
   }
 
   const p = Int54(p1*a + p2);
